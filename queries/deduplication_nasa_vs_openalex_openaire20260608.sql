3203be77b31c78e9e362d4497c2b520e43924ee5
Chun-Kai (Karl) Huang <38848640+huangkarl@users.noreply.github.com>
2026-08-07 16:26:26 +0800

    Create deduplication_nasa_vs_openalex_openaire20260608.sql

---
+++ queries/deduplication_nasa_vs_openalex_openaire20260608.sql
@@ -0,0 +1,30 @@
+WITH
+  
+grant_ids_target AS (
+SELECT
+  UPPER(GRANT_ID) AS GRANT_ID_B,
+  "openalex" AS SOURCE_B  
+FROM `coki-ioi.search_results.results_openalex20260526`
+WHERE
+  "National Aeronautics and Space Administration" IN UNNEST(FUNDER)
+UNION ALL
+SELECT
+  UPPER(GRANT_ID) as GRANT_ID_B,
+  "openaire" AS SOURCE_B
+FROM `coki-ioi.search_results.results_openaire20260608`
+WHERE
+  "National Aeronautics and Space Administration" IN UNNEST(FUNDER)
+)
+
+SELECT
+  table_a.*,
+  SOURCE AS SOURCE_A,
+  GRANT_ID AS GRANT_ID_A,
+  SOURCE_B,
+  GRANT_ID_B
+FROM `coki-ioi.search_results.results_nasa_20260521` AS table_a
+  LEFT JOIN grant_ids_target AS table_b
+  ON (
+    UPPER(GRANT_ID_B) LIKE CONCAT('%',UPPER(GRANT_ID),'%') OR
+    UPPER(GRANT_ID) LIKE CONCAT('%',UPPER(GRANT_ID_B),'%')
+  )
